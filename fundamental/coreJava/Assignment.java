6aefc2d8c4f461959b28fa5c3522174df8c27ac3
Lalit <lalitgupta1qq@gmail.com>
2025-10-21 08:29:11 +0530

    Assignment

---
+++ fundamental/coreJava/Assignment.java
@@ -0,0 +1,11 @@
+package coreJava;
+
+public class Assignment {
+    public static void main(String[] args) {
+        int Myint = 10;
+        System.out.println("Myint is " + Myint);
+        int Newint = Myint;
+        System.out.println("Newint is " + Newint);
+    }
+}
+

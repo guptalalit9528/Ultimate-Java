da402009ad135e775938324fe904da785b2b17d4
Lalit <lalitgupta1qq@gmail.com>
2025-10-21 08:28:48 +0530

    Airthmetic

---
+++ fundamental/coreJava/Airthmetic.java
@@ -0,0 +1,13 @@
+package coreJava;
+
+public class Airthmetic {
+    public static void main(String[] args) {
+        int a=25;
+        int b=4;
+        System.out.println("two number addition:" +(a+b));
+        System.out.println("two number addition:" +(a-b));
+        System.out.println("two number addition:" +(a*b));
+        System.out.println("two number addition:" +(a/b));
+        System.out.println("two number addition:" +(a%b));
+    }
+}

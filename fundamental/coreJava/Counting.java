9e01378189f430ea29d3809f554b9337b43ff063
Lalit <lalitgupta1qq@gmail.com>
2025-10-22 22:55:12 +0530

    Counting

---
+++ fundamental/coreJava/Counting.java
@@ -0,0 +1,19 @@
+package coreJava;
+
+public class Counting {
+    public static void main(String[] args) {
+
+        int i = 1;                          //intialization
+        while (i <= 50) {                  //condition
+                System.out.print(" "+i);  //statement
+                i++;                      //incr/decr
+            }
+        System.out.println(" ");
+        System.out.println("----------------------------------------------");
+        int j = 100;                 //intialization
+        while (j>=50){                 //condition
+        System.out.print(" " + j);   //statement
+        j--;                          //ince/decr
+             }
+    }
+}

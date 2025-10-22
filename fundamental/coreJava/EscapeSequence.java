dd0e22f88a4173b024075ed451dd0e0067fbb334
Lalit <lalitgupta1qq@gmail.com>
2025-10-22 22:56:52 +0530

    Escape Sequence

---
+++ fundamental/coreJava/EscapeSequence.java
@@ -0,0 +1,10 @@
+package coreJava;
+
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****\n*****\n\n");//left half pyramid
+        System.out.println("*****\n****\n***\n**\n*\n\n");//reverse half pyramid
+        System.out.println("    *\n   **\n  ***\n ****\n*****");//right half pyramid
+    }
+
+}

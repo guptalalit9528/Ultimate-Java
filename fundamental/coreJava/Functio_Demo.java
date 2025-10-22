4d5a66746cd7030e18b81d725aa27c863f0daf08
Lalit <lalitgupta1qq@gmail.com>
2025-10-22 22:57:42 +0530

    Function Demo

---
+++ fundamental/coreJava/Functio_Demo.java
@@ -0,0 +1,16 @@
+package coreJava;
+
+public class Functio_Demo {
+    public static void main(String[] args) {
+        System.out.println("hello lalit");
+        greetUser();
+        System.out.println("ab dobara jayega method par");
+        greetUser();
+    }
+
+
+        public static void greetUser(){
+            System.out.println("Good morning lalit");
+        }
+    }
+

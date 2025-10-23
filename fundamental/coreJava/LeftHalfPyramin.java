c6dde288dbedafec78c6cc8167ede27573ca0b0a
Lalit <lalitgupta1qq@gmail.com>
2025-10-23 19:16:33 +0530

    Left Half Pyramin

---
+++ fundamental/coreJava/LeftHalfPyramin.java
@@ -0,0 +1,11 @@
+package coreJava;
+
+public class LeftHalfPyramin {
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   **");
+        System.out.println("  ***");
+        System.out.println(" ****");
+        System.out.println("*****");
+    }
+}

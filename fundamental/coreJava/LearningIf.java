89ffdb5bd51f20816c5ff5c16726c9e476c8e79b
Lalit <lalitgupta1qq@gmail.com>
2025-10-23 19:15:26 +0530

    Learn in glf

---
+++ fundamental/coreJava/LearningIf.java
@@ -0,0 +1,16 @@
+package coreJava;
+
+public class LearningIf {
+    public static void main(String[] args) {
+        boolean isSeniorCitizen = true;
+        boolean isAdult = false;
+
+        if (isSeniorCitizen) {
+            System.out.println("hello Senior citizen");
+        } else if (isAdult) {
+            System.out.println("hallo adult");
+        } else {
+            System.out.println("cycle chalo balak");
+        }
+    }
+}

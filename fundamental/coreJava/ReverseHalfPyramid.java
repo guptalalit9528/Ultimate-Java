9b6555f6b90cbf07bff6f9467289ecf2142e57f7
Lalit <lalitgupta1qq@gmail.com>
2025-10-24 23:46:09 +0530

    Reverse half Pyramid

---
+++ fundamental/coreJava/ReverseHalfPyramid.java
@@ -0,0 +1,11 @@
+package coreJava;
+
+public class ReverseHalfPyramid {
+    public static void main(String[] args) {
+    System.out.println("*****");
+    System.out.println(" ****");
+    System.out.println("  ***");
+    System.out.println("   **");
+    System.out.println("    *");
+}
+}

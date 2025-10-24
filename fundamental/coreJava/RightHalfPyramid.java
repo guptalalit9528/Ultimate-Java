fc1c54ab2053e88b6a040f928b907e53dc4fc711
Lalit <lalitgupta1qq@gmail.com>
2025-10-24 23:46:38 +0530

    Right half Pyramid

---
+++ fundamental/coreJava/RightHalfPyramid.java
@@ -0,0 +1,12 @@
+package coreJava;
+
+public class RightHalfPyramid {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
+

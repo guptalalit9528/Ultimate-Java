6f18d2c8d3cdf91fc3609e96f78bed12402c788a
Lalit <lalitgupta1qq@gmail.com>
2025-11-08 09:14:40 +0530

    Unary operator

---
+++ advance/Lambda/UnaryDemo.java
@@ -0,0 +1,13 @@
+package advance.Lambda;
+
+import java.util.function.UnaryOperator;
+
+public class UnaryDemo {
+    public static void main(String[] args) {
+
+        UnaryOperator<Integer> square = n ->n*n;
+        UnaryOperator<Integer  > increment = n->n+1;
+        var res = increment .andThen(square).apply(1);
+        System.out.println(res);
+    }
+}

56c17078cfbc0a6573b2d2904b526819f4626c5f
Lalit <lalitgupta1qq@gmail.com>
2025-11-09 09:56:43 +0530

    Drive

---
+++ Oops/Drive.java
@@ -0,0 +1,31 @@
+package Oops;
+
+public class Drive {
+    public static void main(String[] args) {
+//        Car myCar=new Car();    //object
+//        myCar.drive();
+//        myCar.addFuel(8);
+//        System.out.println(myCar.getCurrentFuelLevel());
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+//        myCar.addFuel(3);
+//        System.out.println(myCar.getCurrentFuelLevel());
+
+    Car swift=new Car();
+    swift.addFuel(6);
+    swift.start();
+
+    }
+}

f85383fb2290b2864ceacc0342a485e94fcd2e26
Lalit <lalitgupta1qq@gmail.com>
2025-11-09 09:57:08 +0530

    Default constractor

---
+++ Oops/DefaultConstructor.java
@@ -0,0 +1,9 @@
+package Oops;
+
+public class DefaultConstructor {
+
+    public static void main(String[] args) {
+        DefaultConstructor s1=new DefaultConstructor();
+        System.out.println("Default constructor called");
+    }
+    }

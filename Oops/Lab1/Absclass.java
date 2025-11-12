79e28cdc47c35199409fb5990b44c5fe525f672e
Lalit <lalitgupta1qq@gmail.com>
2025-11-12 19:49:24 +0530

    absclass

---
+++ Oops/Lab1/Absclass.java
@@ -0,0 +1,11 @@
+package Oops.Lab1;
+
+public class Absclass {
+    public static void main(String[] args) {
+        Abstraction s1=new Circle();
+        Abstraction s2=new Triangle();
+        s1.draw();
+        s2.draw();
+}
+
+}
